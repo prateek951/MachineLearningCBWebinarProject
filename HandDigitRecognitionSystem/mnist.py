46b09cdba12f96e25b2ea77edc12fb4af22c3769
prateek951 <prince.cenation@gmail.com>
2018-01-09 21:54:21 +0530

    added knn

---
+++ HandDigitRecognitionSystem/mnist.py
@@ -0,0 +1,61 @@
+# Acquire all the core libraries
+import numpy as np 
+import pandas as pd 
+from matplotlib import pyplot as plt 
+
+ds = pd.read_csv('./train.csv')
+print(ds.shape)
+
+data = ds.values 
+print(data.shape)
+# mnist is the dataset of 42000 rows and 785 columns in
+# which first column comprises of all the labels
+# and all remaining 784 columns comprises of training data
+
+# For the labels I want all of the rows and the 0th column
+# For the training data I want all of the rows and the 1st column
+y_train = data[:,0]
+X_train = data[:,1]
+# Print the shape of these
+print(y_train.shape)
+print(X_train.shape)    
+
+# Setup the id of the first figure
+plt.figure(0)
+idx = 104
+print(y_train[idx])     #Print the label at the random index 1204
+# X_train is of the form 784*1 so we want to reshape it to 28*28 and map it into grayscale
+plt.imshow(X_train[idx].reshape((28,28)),cmap='gray')
+plt.show()
+
+def distance(x1,x2):
+    # Here compute the euclidean distance between the two points representing the two vectors
+    return np.sqrt(((x1-x2)**2).sum())
+
+# Basically k is the nearest neighbours count which is generally taken as odd
+# k should not be too high or too low
+# Here small x is the query point
+def knn(X_train,x,y_train,k=5):
+    vals = []
+    # Iterate over the training data and find the distance of all the points in the dataset from the query point x
+    for ix in range(X_train.shape[0]):
+        v = [distance(x,X_train[ix,:]),y_train[ix]]
+        vals.append(v)
+    # Sort all the values that we got
+    updated_vals = sorted(vals,key=lambda x:x[0])
+    # Pick the top k values only
+    pred_arr = np.asarray(updated_vals[:k])
+    pred_arr = np.unique(pred_arr[:,1],return_counts = True)
+    pred = pred_arr[1].argmax()
+    return pred_arr, pred_arr[0][pred]    
+
+idq = int(np.random.random() * X_train.shape[0])
+q = X_train[idq]
+
+res = knn(X_train[:10000],q,y_train[:10000],k=7)
+print(res)
+print(y_train[idq])
+
+plt.figure(0)
+plt.imshow(q.reshape((28,28)),cmap='gray')
+plt.show()
--- knn.py
+++ knn.py
@@ -21,3 +21,69 @@ for x in range(dist_01.shape[0]):
     plt.scatter(dist_02[x,0],dist_02[x,1],color='yellow')
 plt.show()
 
+# Training data preparation
+# 400 samples - 200 apples,200 lemons
+
+labels = np.zeros((400,1))
+# For lemons till 200 we will set it to 0
+# From 200 onwards we have apples so for apples set 1.0
+labels[200:] = 1.0
+
+X_data = np.zeros((400,2))
+# In first 200 rows put the data for apples
+X_data[:200,:] = dist_01
+# In next 200 rows put the data for lemons
+X_data[200:,:] = dist_02
+
+print(X_data)
+print(labels)
+
+# Knn algorithm we are done with the preparation of the dataset
+
+# Distance of query point to all other points in the space
+# O(N) time for every point + sorting time 
+# Complexity for n points is o(Q.N) 
+
+# We need a function to compute distance between 2 points in space
+# These points could be one-d or 2-d or n-d representing a vector
+
+# This gives the euclidean distance between the two points passed in the form of vectors
+def distance(x1,x2):
+    return np.sqrt(((x1-x2)**2).sum())
+
+print(distance(np.array([0.0,0.0]),np.array([1.0,1.0])))
+
+
+# Let us define the KNN 
+# X_train is our training data(dataset)
+# query_point is the one whose membership we are trying to determine
+# y_train is the labels
+# k is 5 which is the nearest neighbours which is generally odd
+# k should not be too high or too low
+
+def knn(X_train,query_point,y_train,k=5):
+    vals=[]
+    # Iterate over each of the points in the dataset
+    for ix in range(X_train.shape[0]):
+        # Compute the distance of the query point from all other points in the dataset
+        v = [distance(query_point,X_train[ix,:]), y_train[ix]]
+        #  Append these points to the vals list
+        vals.append(v)
+        # Now sort the list of all the vals
+        updated_vals = sorted(vals)
+        # Pick up the top k values(talking about the top k points)
+        pred_arr = np.asarray(updated_vals[:k])
+        
+        # Find unique labels
+        pred_arr = np.unique(pred_arr[:,1],return_counts= True)
+        # Largest occurence
+        index = pred_arr[1].argmax()   #Index of largest frequency
+        return pred_arr[0][index]
+       
+q = np.array([1.0,2.0])
+predicted_label = knn(X_data,q,labels)
+print(predicted_label)
+
+
+# Run a loop over the testing data(split it one into the training data and one for the testing data)
+#  
